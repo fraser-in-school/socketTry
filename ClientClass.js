6d4e294040f6ba1b559a0868e130ecb254c716b5
fraser-in-school <1938522559@qq.com>
2019-07-03 11:44:55 +0800

    client add comment

--- ClientClass.js
+++ ClientClass.js
@@ -7,20 +7,26 @@ const {log} = console
 
 
 class ClientClass{
+
+    /*构造器*/
     constructor(port = 133){
-        let self = this;
-        this.username = 'client';
-        this.socket = new io('http://localhost:' + port);
+        let self = this;    //保留对象的上下文
+        this.username = 'client';   //用户名默认为client
+        this.socket = new io('http://localhost:' + port); //默认端口为133
     }
 
+    /*显示的设置用户名*/
     login(username){
         this.username = username;
         //this.socket.emit('login', this);
     }
-    sendMessage(topic = 'chat', message){
-        this.socket.emit(topic, message);
+
+    /*server和单个client通信使用username作为事件名*/
+    sendMessage(topic = this.username, message){
+        this.socket.emit(topic, message); //发送消息
     }
 
+    /*显式的去监听，开始的想法，但其实好像这样的做法不是很好*/
     reciver(topic = this.username){
         this.socket.on(topic, (msg) => {
             log('message: ' + msg);
@@ -28,12 +34,15 @@ class ClientClass{
     }
 }
 
-
+/*使用命令行参数来获得用户名*/
 let arguements = process.argv.splice(2);
+
 let client = new ClientClass();
-client.login(arguements[0]);
+
+client.login(arguements[0]);    //参数绑定为用户名
 client.sendMessage(undefined,'this is client')
 
+/*使用控制台来发送消息*/
 let readline = require('readline');
 let rl = readline.createInterface({
     input:process.stdin,
@@ -41,7 +50,8 @@ let rl = readline.createInterface({
 });
 
 rl.on('line', function (msg) {
-    client.sendMessage(arguements[0], msg)
+    client.sendMessage(undefined, msg)
 })
 
+/*监听默认的端口*/
 client.reciver()
